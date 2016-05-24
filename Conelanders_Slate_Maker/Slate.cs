91a97dff2003126b184e9649432fb978a2559b72
Christopher Winton <GitHubDevSpam@winton.cc>
2016-05-24 18:31:52 -0500

    Removed temporary helper functions.

--- Conelanders_Slate_Maker/Slate.cs
+++ Conelanders_Slate_Maker/Slate.cs
@@ -48,17 +48,64 @@ namespace Conelanders_Slate_Maker {
 
 		}
 
-		public void AddText( string text, string font, int size, int x, int y ) {
-			Font  textFont = new Font( font, size );
-			Point point    = new Point(x, y);
+		//public void AddText( string text, string font, int size, int x, int y ) {
+		public void AddText( string text, LayoutTextFixedSize textInfo ) {
+			Font  textFont = new Font( textInfo.Font.FontFamily, textInfo.Font.Size );
+			Point point    = new Point( textInfo.Coordinate.X, textInfo.Coordinate.Y );
 
 			_Drawing.DrawString( text, textFont, Brushes.White, point );
 
 		}
 
-		//First step make it use types
-		public void AddText( string text, LayoutTextFixedSize textInfo ) {
-			AddText( text, textInfo.Font.FontFamily, textInfo.Font.Size, textInfo.Coordinate.X, textInfo.Coordinate.Y );
+		public void AddText( string text, LayoutTextBounded textInfo ) {
+			Font         textFont = new Font( textInfo.Font.FontFamily, textInfo.Font.Size );
+			var          rect     = new Rectangle( textInfo.BoundingBox.X, textInfo.BoundingBox.Y, textInfo.BoundingBox.Width, textInfo.BoundingBox.Height );
+			StringFormat format   = new StringFormat();
+
+			textFont = GetAdjustedFont( _Drawing, text, textFont, rect.Width, rect.Height, textInfo.Font.Size, 10, false );
+
+			//_Drawing.DrawRectangle( Pens.White, rect );
+			_Drawing.DrawString( text, textFont, Brushes.White, GetCenteredPoint( text, textFont, rect ) );
+
+		}
+
+		public void AddText( string text, LayoutTextFixedOutlined textInfo ) {
+			Font         textFont = new Font( textInfo.Font.FontFamily, textInfo.Font.Size );
+			GraphicsPath path     = new GraphicsPath();
+			Point        point    = new Point( textInfo.Coordinate.X, textInfo.Coordinate.Y );
+			Pen          fatPen   = new Pen( Color.White );
+
+			fatPen.Alignment = PenAlignment.Outset;
+			fatPen.Width     = 5;
+
+			path.AddString( text, textFont.FontFamily, (int) FontStyle.Regular, ( _Drawing.DpiY * textInfo.Font.Size / 72 ), point, new StringFormat() );
+
+			_Drawing.InterpolationMode  = InterpolationMode.High;
+			_Drawing.SmoothingMode      = SmoothingMode.HighQuality;
+			_Drawing.PixelOffsetMode    = PixelOffsetMode.HighQuality;
+			//This only makes it bad apparently(from the few options I tried
+			//_Drawing.TextRenderingHint  = TextRenderingHint.AntiAlias;
+			_Drawing.CompositingQuality = CompositingQuality.HighQuality;
+
+			//_Drawing.DrawPath( Pens.White, path );
+			_Drawing.DrawPath( fatPen, path );
+			_Drawing.FillPath( new SolidBrush(Color.Black), path );
+
+		}
+
+		public void AddImage( string path, TemplatePoint coord ) {
+
+			if( !File.Exists( path ) ) {
+				throw new ArgumentException( String.Format( "Base image could not be found: '{0}'", path ) );
+			}
+
+			var point   = new Point( coord.X, coord.Y );
+			var image   = Image.FromFile( path );
+			var graphic = Graphics.FromImage( image );
+			graphic.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+
+			_Drawing.DrawImage( image, point );
+
 		}
 
 		//This should Autoscale text to fit in a bounding box
@@ -100,79 +147,6 @@ namespace Conelanders_Slate_Maker {
 			//_Drawing.DrawRectangle( new Pen( Color.Red, 1 ), rect.X, rect.Y, size.Width, size.Height );
 
 			return new Point( x, y );
-
-		}
-
-		public void AddText( string text, string font, int size, int x1, int y1, int x2, int y2 ) {
-			Font         textFont = new Font( font, size );
-			var          rect     = new Rectangle( x1, y1, ( x2 - x1 ), ( y2 - y1 ) );
-			StringFormat format   = new StringFormat();
-
-			textFont = GetAdjustedFont( _Drawing, text, textFont, (x2 - x1), (y2 - y1), size, 10, false );
-
-			//_Drawing.DrawRectangle( Pens.White, rect );
-			_Drawing.DrawString( text, textFont, Brushes.White, GetCenteredPoint( text, textFont, rect ) );
-
-		}
-
-		//First step make it use types
-		public void AddText( string text, LayoutTextBounded textInfo ) {
-
-			AddText( text,
-					 textInfo.Font.FontFamily,
-					 textInfo.Font.Size,
-					 textInfo.BoundingBox.X,
-					 textInfo.BoundingBox.Y,
-					 ( textInfo.BoundingBox.X + textInfo.BoundingBox.Width ),
-					 ( textInfo.BoundingBox.Y + textInfo.BoundingBox.Height )
-			);
-
-		}
-
-		public void AddOutLinedText( string text, string font, int size, int x, int y ) {
-			Font         textFont = new Font( font, size );
-			GraphicsPath path     = new GraphicsPath();
-			Point        point    = new Point( x, y );
-			Pen          fatPen   = new Pen( Color.White );
-
-			fatPen.Alignment = PenAlignment.Outset;
-			fatPen.Width     = 5;
-
-			path.AddString( text, textFont.FontFamily, (int) FontStyle.Regular, ( _Drawing.DpiY * size / 72 ), point, new StringFormat() );
-
-			_Drawing.InterpolationMode  = InterpolationMode.High;
-			_Drawing.SmoothingMode      = SmoothingMode.HighQuality;
-			_Drawing.PixelOffsetMode    = PixelOffsetMode.HighQuality;
-			//This only makes it bad apparently(from the few options I tried
-			//_Drawing.TextRenderingHint  = TextRenderingHint.AntiAlias;
-			_Drawing.CompositingQuality = CompositingQuality.HighQuality;
-
-			//_Drawing.DrawPath( Pens.White, path );
-			_Drawing.DrawPath( fatPen, path );
-			_Drawing.FillPath( new SolidBrush(Color.Black), path );
-
-		}
-
-		public void AddText( string text, LayoutTextFixedOutlined textInfo ) {
-			AddOutLinedText( text, textInfo.Font.FontFamily, textInfo.Font.Size, textInfo.Coordinate.X, textInfo.Coordinate.Y );
-		}
-
-		public void AddImage( string path, TemplatePoint coord ) {
-			AddImage( path, coord.X, coord.Y );
-		}
-
-		public void AddImage( string newImage, int x, int y ) {
-
-			if( !File.Exists( newImage ) ) {
-				throw new ArgumentException( String.Format( "Base image could not be found: '{0}'", newImage ) );
-			}
-
-			var point   = new Point(x, y);
-			var image   = Image.FromFile( newImage );
-			var graphic = Graphics.FromImage( image );
-			graphic.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
-
-			_Drawing.DrawImage( image, point );
 
 		}
 
